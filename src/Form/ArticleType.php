7b541dc7579f7cf71cae93df7d58faa1b3ca72c5
Cedric AUDRIT <cedric@audrit.be>
2021-03-09 17:04:19 +0100

    management article

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,23 +15,28 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('author')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => "mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md text-green-900"]
+            ])
+            ->add('author', TextType::class, [
+                'attr' => [
+                    'class' => "mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md text-green-900"]
+            ])
 //            ->add('date_creation')
 //            ->add('date_update')
             ->add('category', ChoiceType::class, [
                 'choices' => $this->getChoices(),
                 'attr' => ['style' => "width:100%;",
-                    'class' => "appearance-none bg-grey-lighter border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-blue-light"]
+                    'class' => "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-green-900"]
             ])
             ->add('intro', TextareaType::class, [
                 'attr' => ['rows' => '3',
-                    'style' => "width:100%;"]
+                    'class' => "shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md text-green-900"]
             ])
             ->add('content', TextareaType::class, [
                 'attr' => ['rows' => '25',
-                    'style' => "width:100%;"]
+                    'class' => "shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md text-green-900"]
             ])
         ;
     }

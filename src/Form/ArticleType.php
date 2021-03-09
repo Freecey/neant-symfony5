fc4e5fdd5824fd63f23021aa271487d4bdb28d0f
Cedric AUDRIT <cedric@audrit.be>
2021-03-09 13:01:36 +0100

    add form style in form view

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,10 +17,21 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('author')
-            ->add('date_creation')
-            ->add('date_update')
-            ->add('category')
-            ->add('intro')
+//            ->add('date_creation')
+//            ->add('date_update')
+            ->add('category', ChoiceType::class, [
+                'choices' => $this->getChoices(),
+                'attr' => ['style' => "width:100%;",
+                    'class' => "appearance-none bg-grey-lighter border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-blue-light"]
+            ])
+            ->add('intro', TextareaType::class, [
+                'attr' => ['rows' => '3',
+                    'style' => "width:100%;"]
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => ['rows' => '25',
+                    'style' => "width:100%;"]
+            ])
         ;
     }
 
@@ -27,5 +40,16 @@ class ArticleType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Article::class,
         ]);
+    }
+
+    private function getChoices()
+    {
+        $choices = Article::CATEGORY;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
+
     }
 }

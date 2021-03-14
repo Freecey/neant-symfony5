edda27b96354acd838cbd4fb04b561eb45a0a136
Cedric AUDRIT <cedric@audrit.be>
2021-03-14 16:28:33 +0100

    user_roles fixed in easy admin

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UsersCrudController extends AbstractCrudController
@@ -21,7 +23,14 @@ class UsersCrudController extends AbstractCrudController
 //            IdField::new('id'),
             TextField::new('alias'),
             TextField::new('email'),
-            ChoiceField::new('roles'),
+//            ArrayField::new('roles'),
+            ChoiceField::new('roles', 'Roles')
+                ->allowMultipleChoices()
+                ->autocomplete()
+                ->setChoices([  'User' => 'ROLE_USER',
+                        'Admin' => 'ROLE_ADMIN',
+                        'SuperAdmin' => 'ROLE_SUPER_ADMIN']
+                )
 //            TextEditorField::new('description'),
         ];
     }

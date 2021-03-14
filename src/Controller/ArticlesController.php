9f43c56833803083af4bbf3139ceca46551ffc71
Cedric AUDRIT <cedric@audrit.be>
2021-03-14 19:16:18 +0100

    start comment

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,8 +2,12 @@
 namespace App\Controller;
 
 use App\Entity\Articles;
+use App\Entity\Comments;
+use App\Entity\Users;
+use App\Form\CommentsType;
 use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -38,7 +42,7 @@ class ArticlesController extends AbstractController
      * @Route("/articles/{id}-{slug}", name="article.show", requirements={"slug": "[a-z0-9\-]*" })
      * @return Response
      */
-    public function show(Articles $article,string $slug): Response
+    public function show(Articles $article,string $slug, Request $request): Response
     {
         if ($article->getSlug() !== $slug){
             return $this->redirectToRoute('article.show', [
@@ -46,9 +50,28 @@ class ArticlesController extends AbstractController
                 'slug' => $article->getSlug()
             ], 301);
         }
+
+        $comment = new Comments();
+//        $comment->addUser($this->getUser());
+        $form = $this->createForm(CommentsType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setArticles($article);
+            $comment->setCreatedAt(new \DateTime('now'));
+//            $comment->addUser($user);
+
+
+            $doctrine = $this->getDoctrine()->getManager();
+            $doctrine->persist($comment);
+            $doctrine->flush();
+        }
+
         return $this->render('article/show.html.twig',[
             'article' => $article,
-            'current_menu' => 'articles'
+            'current_menu' => 'articles',
+            'commentForm' => $form->createView()
         ]);
     }
 

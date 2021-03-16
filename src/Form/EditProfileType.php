dce3935bd7eacfc98a6038dae849e4e276321b78
Cedric AUDRIT <cedric@audrit.be>
2021-03-16 16:06:57 +0100

    add edit profile / change password

--- src/Form/EditProfileType.php
+++ src/Form/EditProfileType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Users;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,22 @@ class EditProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('alias', TextType::class)
+            ->add('email', EmailType::class ,[
+                'attr' => [
+                    'class' => "appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm",
+                    'placeholder' => "email",
+                    'required' ]
+            ])
+            ->add('alias', TextType::class ,[
+                'attr' => [
+                    'class' => "appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm",
+                    'placeholder' => "email",
+                    'required' ]
+            ])
+            ->add('Save', SubmitType::class, [
+                'attr' => [
+                    'class' => "group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"]
+            ])
         ;
     }
 

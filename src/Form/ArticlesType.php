a134a22f5c2dff5a590f48076b9b2728ffcb6717
Cedric AUDRIT <cedric@audrit.be>
2021-03-17 17:43:00 +0100

    profile edit articles

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -4,21 +4,31 @@ namespace App\Form;
 
 use App\Entity\Articles;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class ArticlesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('content')
-            ->add('created_at')
-            ->add('updated_at')
-            ->add('featured_image')
-            ->add('intro')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => "appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"]
+            ])
+            ->add('intro', TextareaType::class, [
+                'attr' => ['rows' => '3',
+                'class' => "appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"]
+            ])
+//            ->add('slug')
+            ->add('content', TextareaType::class)
+//            ->add('created_at')
+//            ->add('updated_at')
+            ->add('imageFile', VichFileType::class)
             ->add('Users')
             ->add('keywords')
             ->add('categories')

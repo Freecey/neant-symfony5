7c75ff301df26a5dee0bc788d335570225d44707
Cedric AUDRIT <cedric@audrit.be>
2021-03-21 14:44:42 +0100

    add liip imagine and apply on home and profile

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
